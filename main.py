1fd2596575009c484c9b73e04ee6bcca9ef2fc60
gulzarsingh1981 <143109638+gulzarsingh1981@users.noreply.github.com>
2024-04-21 19:35:06 -0400

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ print("Gulzar is my name")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCH')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
