042d0a401f20f0e80e1eee1de4b337386a11b68a
gulzarsingh1981 <143109638+gulzarsingh1981@users.noreply.github.com>
2024-04-28 16:27:58 -0400

    Update main.py
    did today

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('gulzar singh the great ')
+    print_hi('gulzar singh greatest ')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
